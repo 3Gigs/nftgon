d40252f46bf82451a56df88677091aa5850695d2
threegigs <yanghuaxuan@gmail.com>
2022-01-28 23:54:25 -0800

    Get rid of imagemagick package

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 'use strict'
 
 const { execSync } = require('child_process');
-const im = require('imagemagick');
-const { exists } = require('fs')
+const { existsSync } = require('fs')
 const { tmpdir } = require('os');
 const { join } = require('path')
 const readline = require('readline');
@@ -12,33 +11,30 @@ const tmp = tmpdir();
 const mask = 'nftMask.png';
 
 rl.question('Enter the path of the image:\n', (answer, err) => {
+    rl.close();
     if(err)
         throw err;
-    rl.close();
+    if(!existsSync(answer))
+        throw new Error("Image cannot be found");
     xd(answer);
 });
 
-// Checks the size of the image, creating using a resized version of the image if needed
 function xd(imgNamePath) {
-    im.identify(imgNamePath, (err, size) => {
-        if(err)
-            throw err;
-        console.log("Checking size...");
-        if(size.width != 400 && size.height != 400)
-        {
-            console.log("❗A Twitter avatar sized (400x400) image is required. Using a resized version of the image...");
-            const imgName = imgNamePath.slice(imgNamePath.search(/[^\\]+$/)).replace(/\.[^\.]+$/, "");
-            const ext = imgNamePath.slice(imgNamePath.search(/\.[^\.]+$/));
-            const tmpImgPath = join(tmp, imgName)+ "_resized" + ext.toString();
-            console.log(tmpImgPath);
-            execSync(`magick ${imgNamePath} -resize 400x400\! ${tmpImgPath}`, (error) => {
-                console.log("Error while resizing");
-                console.error(error);
-            });
-            execSync(`magick ${tmpImgPath} nftMask.png -alpha off -compose copyalpha -composite ${(tmpImgPath.replace(/\.[^\.]+$/, ""))}_nft${tmpImgPath.slice(tmpImgPath.search(/\.[^\.]+$/))}`, (error)=> {
-                console.log("Error while creating NFT");
-                console.error(error)
-            });
-}})}
+    console.log("Checking size...");
+    if(size.width != 400 && size.height != 400)
+    {
+        console.log("❗A Twitter avatar sized (400x400) image is required. Using a resized version of the image...");
+        const imgName = imgNamePath.slice(imgNamePath.search(/[^\\]+$/)).replace(/\.[^\.]+$/, "");
+        const ext = imgNamePath.slice(imgNamePath.search(/\.[^\.]+$/));
+        const tmpImgPath = join(tmp, imgName)+ "_resized" + ext.toString();
+        console.log(tmpImgPath);
+        execSync(`magick ${imgNamePath} -resize 400x400\! ${tmpImgPath}`, (error) => {
+            console.log("Error while resizing");
+            console.error(error);
+        });
+        execSync(`magick ${tmpImgPath} ${mask} -alpha off -compose copyalpha -composite ${(tmpImgPath.replace(/\.[^\.]+$/, ""))}_nft${tmpImgPath.slice(tmpImgPath.search(/\.[^\.]+$/))}`, (error)=> {
+            console.log("Error while creating NFT");
+            console.error(error)
+        });
+    }}
 
-// TODO: Get rid of imagemagick library
