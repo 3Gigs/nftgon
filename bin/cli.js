6a0d257ab22dfcd31ff09c791a99c6d010f4b9d0
threegigs <yanghuaxuan@gmail.com>
2022-02-13 04:20:43 -0800

    CLI

--- bin/cli.js
+++ bin/cli.js
@@ -1,29 +1,51 @@
 #! /usr/bin/env node
 
 const { NFTGon } = require("../");
+const { createWriteStream } = require("fs")
 
-const argv = process.argv.slice(1);
+const argv = process.argv.slice(2);
 
 const args = {
-    inputUrl: "",
+    inputUrls: "",
     outputPath: "",
+    options: "",
 }
 
 for(let i = 0; i < argv.length; i++) {
     switch(argv[i]) {
-        case "-i":
-        case "--input":
-            args.inputUrl = argv[i + 1];
-            i += 1;
+        case "-O":
+        case "--options":
+            args.options = argv[i + 1];
+            i++;
             break;
         case "-o":
         case "--output":
             args.outputPath = argv[i + 1];
-            i += 1;
+            i++;
+            break;
+        // Every arg without an option prefix is appended to input
         default:
+            args.inputUrls += argv[i] + " ";
             break;
     }
 }
 
-console.log(`Input: ${args.inputUrl}`);
-console.log(`Output: ${args.outputPath}`);
+(async () => {
+    const victims = args.inputUrls.split(" ");
+    victims.pop();
+    const out = createWriteStream(args.outputPath.length > 0 
+        ? args.outputPath : (`${__dirname}/a.png`));
+
+    for(input of victims) {
+        const cv = await NFTGon.nftify(input);
+        if(args.options.length > 0) {
+            const opts = JSON.parse(`{${args.options}}`);
+            const png = cv.createPNGStream(opts);
+            png.pipe(out);
+        }
+        else {
+            const png = cv.createPNGStream();
+            png.pipe(out);
+        }
+    }
+})();
